98cf65b14d75fe4e689e806247179a56d86ded3a
Chandu-BG <chandubg5678@gmail.com>
2025-07-25 00:54:26 +0530

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/your-repo-name/',  // Add this line - replace with your actual repo name
+  base: '/green-builds/',  // Add this line - replace with your actual repo name
   server: {
     host: "::",
     port: 8080,
